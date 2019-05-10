e532d58e399ef700cd27881df97c222adab2a6cd
bigphuc <1653068@student.hcmus.edu.vn>
2019-05-10 15:19:18 +0700

    add Entity CRUD

--- controllers/entity.js
+++ controllers/entity.js
@@ -1,69 +1,50 @@
-const Uiza = require('uiza');
+const Uiza = require("uiza");
 let controller = {};
 
 controller.entityList = (req, res) => {
-    Uiza.entity.list()
-        .then((list) => {
-            res.locals.active = 1;
-            res.locals.entityList = list;
-            res.render('entity/listEntity');
-        })
-        .catch((err) => {
-            res.send(err);
-        });
-}
+  Uiza.entity
+    .list()
+    .then(list => {
+      res.locals.active = 1;
+      res.locals.entityList = list;
+      res.render("entity/listEntity");
+    })
+    .catch(err => {
+      res.send(err);
+    });
+};
 
 controller.viewEntity = (req, res) => {
-    var params = {
-        id: req.params.id
-    };
+  var params = {
+    id: req.params.id
+  };
 
-    Uiza.entity.retrieve(params)
-        .then((entity) => {
-            res.locals.entity = entity;
-            res.locals.active = 1;
-            console.log(entity);
-            res.render('entity/playAVideo');
-        })
-        .catch((err) => {
-            res.json(err);
-        })
-}
+  Uiza.entity
+    .retrieve(params)
+    .then(entity => {
+      res.locals.entity = entity;
+      res.locals.active = 1;
+      console.log(entity);
+      res.render("entity/playAVideo");
+    })
+    .catch(err => {
+      res.json(err);
+    });
+};
 
 controller.createEntity = (req, res) => {
-    Uiza.entity.list()
-        .then((list) => {
-            res.locals.active = 1;
-            res.locals.entityList = list;
-            res.render('entity/createEntity');
-        })
-        .catch((err) => {
-            res.send(err);
-        });
-}
+  res.locals.active = 1;
+  res.render('entity/createEntity');
+};
 
 controller.updateEntity = (req, res) => {
-    Uiza.entity.list()
-        .then((list) => {
-            res.locals.active = 1;
-            res.locals.entityList = list;
-            res.render('entity/updateEntity');
-        })
-        .catch((err) => {
-            res.send(err);
-        });
-}
+  res.locals.active = 1;
+  res.render('entity/updateEntity');
+};
 
 controller.deleteEntity = (req, res) => {
-    Uiza.entity.list()
-        .then((list) => {
-            res.locals.active = 1;
-            res.locals.entityList = list;
-            res.render('entity/deleteEntity');
-        })
-        .catch((err) => {
-            res.send(err);
-        });
-}
+  res.locals.active = 1;
+  res.render('entity/deleteEntity');
+};
 
 module.exports = controller;
